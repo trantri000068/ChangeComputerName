469a313030efa81e1d911466cab8249523ffa2d6
trantri000068 <144058992+trantri000068@users.noreply.github.com>
2023-09-05 19:04:56 -0400

    Create ChangeComputerName.ps1

---
+++ ChangeComputerName.ps1
@@ -0,0 +1,43 @@
+# Get the computer's serial number
+$serialNumber = Get-WmiObject -Class Win32_Bios | Select-Object -ExpandProperty SerialNumber
+
+#Declare PCS's computer
+$PCSComputer = "PCS"
+
+#Declare Prefix LP or DP
+$LaptopPrefix = "LP"
+$DesktopPrefix = "DP"
+
+# Get the current computer name
+$currentComputerName = $env:COMPUTERNAME
+
+# Check if it's a laptop or desktop | $computerType.
+
+$computerType = $DesktopPrefix # Default Desktop 
+if ((Get-WmiObject -Class Win32_SystemEnclosure).ChassisTypes -contains 10) {
+    $computerType = $LaptopPrefix  # If it's a laptop, then change it. Note: 10 is Notebook.
+}
+
+# Create new computer name
+$newComputerName = "$PCSComputer-${serialNumber}-$computerType"
+
+If($newComputerName = $currentComputerName){
+    Write-Host "Computer name was already in the correct format!"
+    }
+Else{
+# Set new computer name
+Rename-Computer -NewName $newComputerName -Force
+}
+# Force the computer to restart in a certain time period.
+shutdown.exe -s -t 2400
+New-Item -Path "C:\ChangeComputerNameSuccessful.txt"
+
+#Declare title
+$title = "Computer Name / Domain Name" # Declare title
+
+# Declare message
+$message = "The name of this computer has been updated."
+
+# Display the notification
+Add-Type -AssemblyName PresentationFramework
+[void] [System.Windows.MessageBox]::Show($message, $title,0,64)
